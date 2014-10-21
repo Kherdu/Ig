474b0f4ef25ba0067fe76c57136d11428d67bf44
Kherdu <rafacaturla@gmail.com>
2014-10-21 19:49:23 +0200

    fase 1
    deberia detectar el punto del raton y pintarlo, hay que probar

---
+++ Controller.java
@@ -0,0 +1,181 @@
+//////////////////////////////////////////// 
+// Project skeleton for Computer 2D Graphics
+// MVC-based design
+// Author: Chus Martín
+// 2014
+//////////////////////////////////////////// 
+
+package controller;
+
+//JOGL imports
+import javax.media.opengl.GL;
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLEventListener;
+import javax.media.opengl.GL2;
+import javax.media.opengl.fixedfunc.GLMatrixFunc;
+import javax.media.opengl.glu.GLU;
+
+//AWT imports
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+//Specific imports
+import model.Scene;
+
+public class Controller implements GLEventListener, KeyListener, MouseListener{
+
+	private Scene scene; // The model which is controlled  
+	private GLAutoDrawable canvas; // The viewport that is used to render the model
+	
+	private final GLU glu = new GLU(); //This object is kept for invoking gluOrtho2D in update_PROJECTION_MATRIX
+	
+	public Controller(GLAutoDrawable canvas1){
+		System.out.print("Into Controller's constructor\n\n");
+		
+		canvas= canvas1;		      
+		double xLeft, xRight, yTop, yBottom;
+		xLeft=0; xRight= (double)canvas.getWidth();
+        yBottom=0;  yTop= (double)canvas.getHeight();
+		scene= new Scene(xLeft, xRight, yTop, yBottom); //Initialize the scene size with the viewport size 
+	    
+		System.out.print("Scene bounds:\n");
+	    System.out.print("xLeft:  \t" + xLeft +   " xRight:\t" +  xRight + "\n");
+	    System.out.print("yBottom:\t" + yBottom + " yTop:  \t" +  yTop + "\n");
+	    System.out.print("\n");
+
+    }
+	
+		
+	/////////////////////////////////
+	// GLEventListener implementation
+	@Override
+	public void init(GLAutoDrawable drawable) {
+		System.out.print("Into init\n\n");
+		
+		GL2 gl = drawable.getGL().getGL2();
+
+		gl.glClearColor(1, 1, 1, 1);
+		gl.glColor3f(0.0f,0.0f,1.0f); 
+
+		gl.glPointSize(4.0f);
+		gl.glLineWidth(2.0f);
+
+		update_PROJECTION_MATRIX(drawable);
+		
+		gl.glMatrixMode(GLMatrixFunc.GL_MODELVIEW);
+		gl.glLoadIdentity();
+	}
+
+	@Override
+	public void dispose(GLAutoDrawable drawable) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void display(GLAutoDrawable drawable) {
+		System.out.print("Into display\n");
+		GL2 gl = drawable.getGL().getGL2();
+        gl.glClear(GL.GL_COLOR_BUFFER_BIT);
+		scene.draw(drawable);
+	}
+
+	@Override
+	public void reshape(GLAutoDrawable drawable, int x, int y, int width,
+			int height) {
+
+		double viewPortRatio= (double)width/(double)height;		
+		scene.resize(viewPortRatio);
+		update_PROJECTION_MATRIX(drawable);
+	}
+		
+
+	/////////////////////////////////
+	// KeyListener implementation
+	@Override
+	public void keyPressed(KeyEvent e) {
+		switch(e.getKeyCode()){
+			case KeyEvent.VK_RIGHT: scene.moveTriangle(10,0); break;
+			case KeyEvent.VK_LEFT:  scene.moveTriangle(-10,0); break;
+			case KeyEvent.VK_UP:  scene.moveTriangle(0,10); break;
+			case KeyEvent.VK_DOWN:  scene.moveTriangle(0,-10); break;
+		}
+		
+		canvas.display();
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+	/////////////////////////////////
+	// MouseListener implementation
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		
+		double posx=e.getX();
+		double posy=e.getY();
+		
+		scene.mouseDot(posx, posy);
+		canvas.display();	
+		
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	//////////////////////////////////
+	// Specific methods for this class
+	public void move(double xShift,double yShift){
+		scene.moveTriangle(xShift,yShift);
+		canvas.display();
+	}
+	
+	private void update_PROJECTION_MATRIX(GLAutoDrawable drawable) {
+		double xLeft= scene.getXLeft();
+		double xRight= scene.getXRight();
+		double yBottom= scene.getYBottom();
+		double yTop= scene.getYTop();
+				
+		GL2 gl = drawable.getGL().getGL2();
+		gl.glMatrixMode(GLMatrixFunc.GL_PROJECTION);
+        gl.glLoadIdentity();
+		glu.gluOrtho2D(xLeft, xRight, yBottom, yTop);
+	}
+
+	
+}
---
+++ Scene.java
@@ -0,0 +1,125 @@
+//////////////////////////////////////////// 
+// Project skeleton for Computer 2D Graphics
+// MVC-based design
+// Author: Chus Martín
+// 2014
+//////////////////////////////////////////// 
+
+package model;
+
+//JOGL imports
+import javax.media.opengl.GL;
+import javax.media.opengl.GL2;
+import javax.media.opengl.GLAutoDrawable;
+
+public class Scene {
+
+	// Scene Visible Area (SVA)
+	private double xLeft, xRight, yTop, yBottom; // SVA position
+	
+	// Scene variables
+	private double xTriangle, yTriangle;
+	private double triangleWidth, triangleHeight;
+	private boolean redColor;
+	private double Mx,My;
+	
+	/////////////////////////////////
+	public Scene(double xLeft1, double xRight1, double yTop1, double yBottom1){
+		// SVA
+		xLeft= xLeft1;
+		xRight= xRight1;
+		yTop= yTop1;
+		yBottom= yBottom1;
+		
+		//mouse coordinates
+		Mx=0;
+		My=0;
+		
+		// Triangle size
+		triangleWidth= 0.4*(xRight-xLeft);
+        triangleHeight= 0.8*(yTop-yBottom);
+        // Triangle initial location
+        xTriangle= xLeft + 0.3*(xRight-xLeft);
+        yTriangle= yBottom + 0.1*(yTop-yBottom);	
+        
+        redColor= true;
+	     
+	}
+	
+	/////////////////////////////////
+	public double getXLeft()   { return xLeft;}
+	public double getXRight()  { return xRight;}
+	public double getYTop()    { return yTop;}
+	public double getYBottom() { return yBottom;}
+	public double getWidth()   { return xRight-xLeft;}
+	public double getHeight()  { return yTop-yBottom;}
+	
+    /////////////////////////////////
+	public void resize(double viewPortRatio){		
+		double sceneVisibleAreaRatio=(xRight-xLeft)/(yTop-yBottom);
+		
+		if (sceneVisibleAreaRatio>=viewPortRatio){
+			     // Increase SVA height
+			     double newHight= (xRight-xLeft)/viewPortRatio;
+			     double yCenter= (yBottom+yTop)/2.0;
+			     yTop= yCenter + newHight/2.0;
+			     yBottom= yCenter - newHight/2.0;
+		}
+		else{
+				// Increase SVA width
+				double newWidth= viewPortRatio*(yTop-yBottom);
+				double xCenter= (xLeft+xRight)/2.0;
+				xRight= xCenter + newWidth/2.0;
+				xLeft= xCenter - newWidth/2.0;
+		}
+	}
+
+	/////////////////////////////////
+	public void draw(GLAutoDrawable drawable){
+		
+		GL2 gl = drawable.getGL().getGL2();
+        
+        if(redColor) gl.glColor3f(1.0f,0.0f,0.0f);
+        else gl.glColor3f(0.0f,1.0f,0.0f); 
+
+        
+        gl.glBegin(GL.GL_LINE_LOOP);
+        	gl.glVertex2d( xTriangle, yTriangle );
+	        gl.glVertex2d( xTriangle + triangleWidth, yTriangle );
+	        gl.glVertex2d( xTriangle + triangleWidth/2.0, yTriangle + triangleHeight );
+        gl.glEnd();
+
+        gl.glBegin(GL.GL_POINTS);
+			gl.glVertex2d(Mx, My);
+		gl.glEnd();
+        	
+    
+       
+        
+        gl.glFlush();
+	}
+	
+	
+	
+	
+	/////////////////////////////////
+	public void moveTriangle(double xShift, double yShift){
+		xTriangle += xShift;
+		yTriangle += yShift;
+	}
+	
+	
+	public void mouseDot(double mouseX, double mouseY){
+		
+		My=mouseX;
+		Mx=mouseY;
+		
+		
+		
+	}
+	/////////////////////////////////
+	public void changeColor(){
+		redColor = !redColor;
+	}
+
+}
